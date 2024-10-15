9bb2c4c69b66cd951161d45f6e77c69b79f0e234
scc-kurihara <kurihara.m@scc-net.co.jp>
2024-10-15 15:43:51 +0900

    構成画面：画面サイズ等の変更

--- Prototype/Prototype1/resource.h
+++ Prototype/Prototype1/resource.h
@@ -24,6 +24,9 @@
 #define IDC_LIST1                       1004
 #define IDC_LIST_CONFIG                 1004
 #define IDC_STATIC_UNITBASE             1005
+#define IDC_STATIC_MODELNAME            1006
+#define IDC_STATIC_UNITLIST             1007
+#define IDC_STATIC_NUMBERUNIT           1008
 
 // Next default values for new objects
 // 
@@ -31,7 +34,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        139
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_CONTROL_VALUE         1009
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
