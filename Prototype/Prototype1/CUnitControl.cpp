a1af509165a684294758b0a34ee2215a39529ec3
tatsuya-tezuka <tatsuya.tezuka@gmail.com>
2024-10-23 10:03:53 +0900

    ユニット削除処理の追加

--- Prototype/Prototype1/CUnitControl.cpp
+++ Prototype/Prototype1/CUnitControl.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
+#include "Prototype1.h"
 #include "CUnitControl.h"
+#include "CUnitControlBase.h"
 
 /*
 	ユニットコントロール
@@ -15,6 +17,8 @@ CUnitControl::~CUnitControl()
 
 BEGIN_MESSAGE_MAP(CUnitControl, CStatic)
 	ON_WM_SETCURSOR()
+	ON_WM_CONTEXTMENU()
+	ON_COMMAND(ID_UNIT_DELETE, &CUnitControl::OnUnitDelete)
 END_MESSAGE_MAP()
 
 /*============================================================================*/
@@ -53,4 +57,28 @@ void CUnitControl::PreSubclassWindow()
 	::SetWindowLong(GetSafeHwnd(), GWL_STYLE, dwStyle | SS_NOTIFY);
 
 	CStatic::PreSubclassWindow();
+}
+
+
+void CUnitControl::OnContextMenu(CWnd* pWnd, CPoint point)
+{
+	if (mType == CUnitControlBase::UnitBracket || mType == CUnitControlBase::UnitEmpty) {
+		// 空ユニットの場合は何もしない
+		return;
+	}
+
+	// 削除メニューの表示
+	CMenu menu;
+	if (menu.LoadMenu(IDR_MENU_DELETE)){
+		CMenu* pSubMenu = menu.GetSubMenu(0);
+		if (pSubMenu == NULL)
+			return;
+		pSubMenu->TrackPopupMenu(TPM_LEFTALIGN | TPM_RIGHTBUTTON, point.x, point.y, this);
+	}
+}
+
+
+void CUnitControl::OnUnitDelete()
+{
+	GetParent()->PostMessage(mMessage_UnitDelete, (WPARAM)GetDlgCtrlID(), 0);
 }
--- Prototype/Prototype1/CUnitControlBase.cpp
+++ Prototype/Prototype1/CUnitControlBase.cpp
@@ -1,4 +1,5 @@
 #include "pch.h"
+#include "Prototype1.h"
 #include "CUnitControlBase.h"
 
 /*
